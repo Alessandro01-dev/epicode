b698f51f32c1f65e57624d6f9ce57e8f2c7eca32
Alessandro Aronica <ale.aronica@outlook.it>
2025-09-16 20:42:17 +0200

    renamed M3/W3D1 practice folder

---
+++ M3/W3D1 practice/script.js
@@ -0,0 +1,59 @@
+/* integrazione di altre features
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+*/
+/*
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+*/
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 8,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  }
+});
