43494697be8a92583abca010bfaa293d0e1ddf8e
Alessandro Aronica <ale.aronica@outlook.it>
2025-11-01 19:15:05 +0100

    updated M4 W4D1

--- M4/W4D1/main.js
+++ M4/W4D1/main.js
@@ -1,5 +1,6 @@
 const searchInput = document.querySelector(".search-container input");
 const searchIcon = document.querySelector(".search-icon-container i");
+const productsContainer = document.getElementById("products-container");
 
 const hideAndShowSearchInput = () => {
 
@@ -55,8 +56,38 @@ const createProduct = async (obj) => {
 
 }
 
+const deleteProduct = async (event) => {
+
+  const productId = event.target.dataset.id
+
+  try {
+
+    const response = await fetch(`${URLData}${productId}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGViZGRkNjQ3N2VkYTAwMTU3MzU2OTUiLCJpYXQiOjE3NjE5OTkyMzcsImV4cCI6MTc2MzIwODgzN30.EpWRymcY9re_KLNkZ37E4Y34r6XWl2123tMzMX7y_l4"
+      }
+    })
+
+    updateProducts()
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateProducts = async () => {
+
+  const updatedProducts = await getProducts()
+
+  productsContainer.innerHTML = ""
+
+  updatedProducts.forEach(updatedProduct => createProductCard(updatedProduct, productsContainer))
+
+}
+
 const newProduct = {
-  name: "4610 cellphone",
+  name: "5010 cellphone",
   description: "An unforgettable icon.",
   brand: "Nokia",
   imageUrl: "https://bit.ly/3CExjRa",
@@ -65,5 +96,59 @@ const newProduct = {
 
 createProduct(newProduct)
 
+const createProductCard = (product, container) => {
+
+  const productCardWrapper = document.createElement("div")
+  productCardWrapper.setAttribute("class", "col-6 col-md-4 col-lg-3")
+  container.appendChild(productCardWrapper)
+
+  const productCardContainer = document.createElement("div")
+  productCardContainer.setAttribute("class", "d-flex flex-column gap-2 align-items-start product-card-container position-relative")
+  productCardWrapper.appendChild(productCardContainer)
+
+  const productCardImgContainer = document.createElement("div")
+  productCardContainer.appendChild(productCardImgContainer)
+
+  const productCardImg = document.createElement("img")
+  productCardImg.setAttribute("class", "w-100 object-fit-cover")
+  productCardImg.src = product.imageUrl
+  productCardImg.alt = product.name
+  productCardImgContainer.appendChild(productCardImg)
+
+  const productCardTitle = document.createElement("h5")
+  productCardTitle.innerText = product.name
+  productCardContainer.appendChild(productCardTitle)
+
+  const productCardDescription = document.createElement("p")
+  productCardDescription.innerText = product.description
+  productCardContainer.appendChild(productCardDescription)
+
+  const productCardBottomSectionContainer = document.createElement("div")
+  productCardBottomSectionContainer.setAttribute("class", "d-flex justify-content-between w-100")
+  productCardContainer.appendChild(productCardBottomSectionContainer)
+
+  const productCardBrand = document.createElement("p")
+  productCardBrand.setAttribute("class", "badge rounded-pill text-bg-dark")
+  productCardBrand.innerText = product.brand
+  productCardBottomSectionContainer.appendChild(productCardBrand)
+
+  const productCardPrice = document.createElement("p")
+  productCardPrice.classList.add("product-card-price")
+  productCardPrice.innerText = `${product.price} $`
+  productCardBottomSectionContainer.appendChild(productCardPrice)
+
+  const productCardDeleteProduct = document.createElement("div")
+  productCardDeleteProduct.setAttribute("class", "btn-close position-absolute top-0 end-0")
+  productCardDeleteProduct.dataset.id = product._id
+  productCardContainer.appendChild(productCardDeleteProduct)
+  productCardDeleteProduct.addEventListener("click", deleteProduct)
+
+}
+
 getProducts()
-  .then(res => console.log(res))
+  .then(res => {
+    res.forEach(product => {
+      createProductCard(product, productsContainer)
+    });
+  })
+
