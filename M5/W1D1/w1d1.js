2f018b51e6decce9cadd5816a8c9d0f51bd09184
Alessandro Aronica <ale.aronica@outlook.it>
2025-11-12 23:29:12 +0100

    uploaded M5/W1D1 folder

---
+++ M5/W1D1/w1d1.js
@@ -0,0 +1,75 @@
+// DOMANDA 1
+/*
+Cos'è ReactJS?
+
+ReactJS è una libreria esterna creata per sviluppare più facilmente interfacce grafiche.
+
+Si può avviare un progetto attraverso npm (node package manager). La toolchain command è: npm create vite@latest [nome del progetto].
+
+Vite mette a disposizione un boilerplate, cioè un codice standard e riutilizzabile.
+
+All'interno dei file è presente un file .html che React cambia in base ai file .jsx e .css che si importano all'interno della propria App. Per questo motivo React si definisce come una single page app.
+*/
+
+// DOMANDA 2
+/*
+ReactJS è un framework, non una libreria.
+
+Falso
+*/
+
+// DOMANDA 3
+/*
+Il file package.json contiene...
+
+Molte informazioni utili, come ad esempio l'elenco di tutte le dipendenze richieste dall'applicazione.
+*/
+
+// DOMANDA 4
+/*
+create-react-app è l'unico modo possibile per creare un'applicazione React.
+
+Falso
+*/
+
+// DOMANDA 5
+/*
+Qual è il comando da lanciare nel teminale per creare una nuova crate-react-app con nome "test"?
+
+npm create-react-app-test
+*/
+
+// DOMANDA 6
+/*
+Cos'è un componente React?
+
+Un blocco di logica/contenuto riutilizzabile all'interno dell'applicazione
+*/
+
+// DOMANDA 7
+/*
+Un componente React può venire creato in tre modi: come funzione, classe o interfaccia.
+
+Falso
+*/
+
+// DOMANDA 8
+/*
+Le props sono frammenti di informazione assegnati all'invocazione di un componente React, utili al fine di rendere il componente dinamico e più riutilizzabile.
+
+Vero
+*/
+
+// DOMANDA 9
+/*
+Le props possono essere passate solamente da un componente genitore ad un componente figlio, non è possibile fare il contrario
+
+Vero
+*/
+
+// DOMANDA 10
+/*
+Da dove possono venire recuperate le props all'interno di un componente React creato come classe?
+
+Possono essere recuperate all’interno dello oggetto ‘this’, dentro un sotto-oggetto chiamato ‘props’
+*/
