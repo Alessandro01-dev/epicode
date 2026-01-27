2a7c65b12fd3a9507dbc98d1661d63f8b5a7b7bd
Alessandro Aronica <ale.aronica@outlook.it>
2026-01-27 02:49:09 +0100

    updated Strive blog: added OAuthRedirectHandler.css

--- M6/Strive blog/src/middlewares/OAuthRedirectHandler.jsx
+++ M6/Strive blog/src/middlewares/OAuthRedirectHandler.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Alert, Container, Row, Col } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
+import "./OAuthRedirectHandler.css"
 
 const OAuthRedirectHandler = () => {
   const navigate = useNavigate();
@@ -29,22 +30,22 @@ const OAuthRedirectHandler = () => {
             className="d-flex flex-column align-items-center"
           >
             <Alert
-            className="w-50 text-center"
-            variant="success"
-          >
-            Success! You're logged in
-          </Alert>
-          <div
-            className="w-25 py-3"
-          >
-            <img
-              className="w-100 object-fit-cover d-block"
-              src="https://cdn-icons-png.flaticon.com/128/5709/5709755.png"
-              alt="oauth login success icon" />
-          </div>
-          <h1
-            className="text-center"
-          >You're being redirecting...</h1>
+              className="w-50 text-center"
+              variant="success"
+            >
+              Success! You're logged in
+            </Alert>
+            <div
+              className="oauth-login-success-icon-container py-3"
+            >
+              <img
+                className="w-100 object-fit-cover d-block"
+                src="https://cdn-icons-png.flaticon.com/128/5709/5709755.png"
+                alt="oauth login success icon" />
+            </div>
+            <h1
+              className="text-center"
+            >You're being redirecting...</h1>
           </div>
         </Col>
       </Row>
